3588d96b9bb50da5ca9ae1a07884f40c77bf957e
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-02-15 11:02:20 +0100

    changes

---
+++ typescript/script.js
@@ -0,0 +1,9 @@
+"use strict";
+let text = "Hello world!";
+console.log(text);
+let text2 = 42;
+console.log(text);
+let text3 = 42;
+console.log(text);
+let text4 = "Hello world!";
+console.log(text);
---
+++ typescript/script.ts
@@ -0,0 +1,8 @@
+let text : string = "Hello world!";
+console.log(text);
+let text2 : number = 42;
+console.log(text);
+let text3 : number = 42;
+console.log(text);
+let text4 : string = "Hello world!";
+console.log(text);
